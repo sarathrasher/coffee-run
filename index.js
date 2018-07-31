f002e0a093c351c81da4f2026f968926fc370b00
Sara Muntean <munteansr@gmail.com>
2018-07-31 09:02:35 -0400

    Fixed issue where removing order removed entire order list.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var orderForm = document.querySelector('[data-coffee-order="form"]');
 var orderList = document.querySelector(".order-list");
-var checkbox = document.createElement('input');
-checkbox.setAttribute('type', 'checkbox');
+
 var orders = [];
 
 var submit = function (event) {
@@ -23,12 +22,14 @@ var submit = function (event) {
 }
 
 var printOrder = function (order) {
-    var orderListItem = document.createElement('li')
-    orderListItem.classList.add('order')
-
     orderStatement = `${order.coffeeOrder}, ${order.email}, ${order.size}, ${order.flavor}, ${order.strength}`
 
+    var orderListItem = document.createElement('li')
+    orderListItem.classList.add('order')
     orderListItem.textContent = orderStatement
+
+    var checkbox = document.createElement('input');
+    checkbox.setAttribute('type', 'checkbox');
 
     orderListItem.appendChild(checkbox);
     orderList.appendChild(orderListItem);
@@ -40,7 +41,7 @@ var printOrder = function (order) {
     checkbox.addEventListener('click', removeOrder);
 };
 
-orders.forEach(function(order) {printOrder(order)});
+// localStorage.getItem('coffeeOrders').forEach(function(order) {printOrder(order)});
 
 orderForm.addEventListener('submit', submit);
 
