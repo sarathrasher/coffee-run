423e538cecf00ea716cf18d087eec242f1360d83
Sara Muntean <munteansr@gmail.com>
2018-07-31 15:29:34 -0400

    Utilized ajax to populate order data from a server

--- index.js
+++ index.js
@@ -3,23 +3,10 @@ var orderList = document.querySelector(".order-list");
 
 var orders = [];
 
-var setLocalStorage = function () {
-    localStorage.setItem("coffeeOrders", JSON.stringify(orders));
-};
-
-var deleteByValue = function (value) {
-    var newOrderList = []
-    for (var i = 0; i < orders.length; i++) {
-        if (orders[i] !== value) {
-            newOrderList.push(orders[i]);
-        }
-    }
-    orders = newOrderList;
-    setLocalStorage();
-};
+var url = 'https://dc-coffeerun.herokuapp.com/api/coffeeorders'
 
 var printOrder = function (order) {
-    var orderText = `${order.coffeeOrder}, ${order.email}, ${order.size}, ${order.flavor}, ${order.strength}`
+    var orderText = `${order.coffee}, ${order.emailAddress}, ${order.size}, ${order.flavor}, ${order.strength}`
 
     var orderListItem = document.createElement('li')
     orderListItem.classList.add('order')
@@ -41,44 +28,56 @@ var printOrder = function (order) {
     checkbox.addEventListener('click', removeOrder);
 };
 
+var getData = function () {
+    $.ajax(url, {
+        success: function(coffeeOrders) {
+            Object.values(coffeeOrders).forEach(function(order) {
+                printOrder(order)
+            });
+            console.log(Object.values(coffeeOrders));
+        },
+        error: function() {
+            console.log('boom!');
+        }
+    })
+};
+
+var post = function (orderObject) {
+    $.ajax(url, {
+        method: 'POST',
+        data: { orderObject },
+        success: getData()
+        })
+};
+
+var deleteByValue = function (value) {
+    var newOrderList = []
+    for (var i = 0; i < orders.length; i++) {
+        if (orders[i] !== value) {
+            newOrderList.push(orders[i]);
+        }
+    }
+    orders = newOrderList;
+    getData();
+};
+
 var submit = function (event) {
     event.preventDefault();
 
-    var coffeeOrder = document.querySelector('[name="coffee"]');
+    var coffee = document.querySelector('[name="coffee"]');
     var email = document.querySelector('[name="emailAddress"]');
     var size = document.querySelector('[name="size"]:checked');
     var flavor = document.querySelector('[name="flavor"]');
     var strength = document.querySelector('[name="strength"]')
 
-    var orderObject = {coffeeOrder: coffeeOrder.value, email: email.value, size: size.value, flavor: flavor.value, strength: strength.value};
+    var orderObject = {coffee: coffee.value, email: email.value, size: size.value, flavor: flavor.value, strength: strength.value};
 
     orders.push(orderObject);
-
-    printOrder(orderObject); 
-
-    setLocalStorage(orders);
+    
+    post(orderObject);
 };
 
-orders = JSON.parse(localStorage.getItem('coffeeOrders'))
-if (orders !== null) {
-    orders.forEach(function(order) {printOrder(order)});
-} else if (orders === null) {
-    orders = [];
-}
-
+getData();
 orderForm.addEventListener('submit', submit);
 
-// $.ajax('https://dc-coffeerun.herokuapp.com/api/coffeeorders', {
-//     success: function(coffeeOrders) {
-//         console.log(coffeeOrders);
-//     },
-//     error: function() {
-//         console.log('boom!');
-//     }
-// })
-
-// method: 'POST', 
-// data: { emailAddress: '',
-//         coffee: '',
-//     }
 
