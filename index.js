aa31da0bb86f8406d38772f3ae27d8dce306ea94
Sara Muntean <munteansr@gmail.com>
2018-07-31 10:19:09 -0400

    Added functionality to remove item from local storage.

--- index.js
+++ index.js
@@ -3,23 +3,17 @@ var orderList = document.querySelector(".order-list");
 
 var orders = [];
 
-var submit = function (event) {
-    event.preventDefault();
-
-    var coffeeOrder = document.querySelector('[name="coffee"]');
-    var email = document.querySelector('[name="emailAddress"]');
-    var size = document.querySelector('[name="size"]:checked');
-    var flavor = document.querySelector('[name="flavor"]');
-    var strength = document.querySelector('[name="strength"]')
-
-    var orderObject = { coffeeOrder: coffeeOrder.value, email: email.value, size: size.value, flavor: flavor.value, strength: strength.value};
-
-    orders.push(orderObject);
-
-    printOrder(orderObject); 
-
+var setLocalStorage = function (order) {
     localStorage.setItem("coffeeOrders", JSON.stringify(orders));
-}
+};
+
+var deleteByValue = function (value) {
+    for (var i = 0; i < orders.length; i++) {
+        if (orders[i] === value) {
+            orders.splice(orders[i]);
+        };
+    };
+};
 
 var printOrder = function (order) {
     orderStatement = `${order.coffeeOrder}, ${order.email}, ${order.size}, ${order.flavor}, ${order.strength}`
@@ -36,12 +30,34 @@ var printOrder = function (order) {
 
     var removeOrder = function (event) {
         orderList.removeChild(orderListItem)
+        deleteByValue(order)
+        setLocalStorage(orders);
     };
 
+    setLocalStorage(orders);
     checkbox.addEventListener('click', removeOrder);
 };
 
-// localStorage.getItem('coffeeOrders').forEach(function(order) {printOrder(order)});
+var submit = function (event) {
+    event.preventDefault();
+
+    var coffeeOrder = document.querySelector('[name="coffee"]');
+    var email = document.querySelector('[name="emailAddress"]');
+    var size = document.querySelector('[name="size"]:checked');
+    var flavor = document.querySelector('[name="flavor"]');
+    var strength = document.querySelector('[name="strength"]')
+
+    var orderObject = {coffeeOrder: coffeeOrder.value, email: email.value, size: size.value, flavor: flavor.value, strength: strength.value};
+
+    orders.push(orderObject);
+
+    printOrder(orderObject); 
+
+    setLocalStorage(orders);
+}
+
+    JSON.parse(localStorage.getItem('coffeeOrders')).forEach(function(order) {printOrder(order)});
+
 
 orderForm.addEventListener('submit', submit);
 
